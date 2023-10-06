2edb852de700566c3142d5756508021bbf662215
ivan <imysin@mail.ru>
2023-10-06 19:51:03 +0300

    orgs is start to develope

--- school_site/main/urls.py
+++ school_site/main/urls.py
@@ -5,11 +5,14 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("", views.index, name='home'),
-    path("about_us", views.about, name='about_us'),
-    path("dates", views.about, name='dates'),
-    path("orgs", views.about, name='orgs'),
-    path("lodging", views.about, name='lodging'),
-    path("payment", views.about, name='payment'),
-    path("contacts", views.about, name='contacts'),
-    path("send_application", views.about, name='send_application'),
+    path("dates", views.index, name='dates'),
+    path("orgs", views.orgs, name='orgs'),
+    path("lodging", views.index, name='lodging'),
+    path("payment", views.index, name='payment'),
+    path("contacts", views.index, name='contacts'),
+    path("send_application", views.index, name='send_application'),
+    path("partners", views.index, name='partners'),
+    path("history", views.index, name='history'),
+    path("faqs", views.index, name='faqs'),
+    path("lectors", views.index, name='lectors'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- school_site/main/views.py
+++ school_site/main/views.py
@@ -1,10 +1,39 @@
 from django.shortcuts import render
-
+import os
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    filenames = []
+    for filename in os.listdir('/home/ivan/PycharmProjects/SchoolSite/school_site/main/static/main/images/home_slider'):
+        if filename[-4:] != '.jpg': continue
+        filenames.append(filename)
 
-def about(request):
-    return render(request, 'main/index.html')
+
+    data4render = {
+        'text2title': 'О школе',
+        'text2' : "Летняя школа \"Введение в нейробиологию внимания и памяти\" — ежегодное мероприятие. Цель школы - привлечение студентов в нейронауку. В рамках школы проходят лекции ведущих ученых, практические занятия и нейротурнир",
+        'text1title' : 'Наша миссия',
+        'text1' : ["Наша летняя научная школа предназначена для того, чтобы помочь студентам развить навыки научного исследования и критического мышления.",
+                   "Мы стремимся предоставить студентам возможность погрузиться в мир науки и исследований, а также научиться работать в команде.",
+                   "Наша летняя научная школа предлагает разнообразные курсы по различным областям научных знаний, включая физику, биологию и математику.",
+                   "Мы считаем, что научное исследование имеет огромное значение для развития общества и будущего нашей планеты.",
+                   "Мы стремимся создать дружественную и поддерживающую среду для всех участников, где они могут развивать свои таланты и достигать своих целей."],
+        'filenames' : filenames,
+    }
+    return render(request, 'main/index.html', data4render)
+
+def orgs(request):
+    data4render = []
+
+    for i in range(15):
+        org = {
+            "name" : 'Имя',
+            "surname" : 'Фамилия',
+            "role" : 'За что человек отвечает',
+            "photo" : '',
+        }
+        data4render.append(org)
+
+
+    return render(request, 'main/orgs.html', {'orgs' : data4render})
