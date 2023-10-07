9047faa45b70ce0f6a6aba97a4c6e9347196f402
ivan <imysin@mail.ru>
2023-10-07 18:53:22 +0300

    Only faqs is need to develope

--- school_site/main/urls.py
+++ school_site/main/urls.py
@@ -5,14 +5,16 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("", views.index, name='home'),
-    path("dates", views.index, name='dates'),
+    path("dates", views.dates, name='dates'),
     path("orgs", views.orgs, name='orgs'),
-    path("lodging", views.index, name='lodging'),
-    path("payment", views.index, name='payment'),
-    path("contacts", views.index, name='contacts'),
-    path("send_application", views.index, name='send_application'),
-    path("partners", views.index, name='partners'),
-    path("history", views.index, name='history'),
-    path("faqs", views.index, name='faqs'),
-    path("lectors", views.index, name='lectors'),
+    path("lectors", views.orgs, name='lectors'),
+    path("partners", views.orgs, name='partners'),
+    path("lodging", views.text, name='lodging'),
+    path("payment", views.text, name='payment'),
+    path("contacts", views.text, name='contacts'),
+    path("send_application", views.text, name='send_application'),
+
+    path("history", views.history, name='history'),
+    path("faqs", views.faqs, name='faqs'),
+
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- school_site/main/views.py
+++ school_site/main/views.py
@@ -4,6 +4,8 @@ import os
 
 
 def index(request):
+
+
     filenames = []
     for filename in os.listdir('/home/ivan/PycharmProjects/SchoolSite/school_site/main/static/main/images/home_slider'):
         if filename[-4:] != '.jpg': continue
@@ -25,15 +27,84 @@ def index(request):
 
 def orgs(request):
     data4render = []
-
     for i in range(15):
         org = {
             "name" : 'Имя',
-            "surname" : 'Фамилия',
+            "surname" : 'Фамилия Отчество',
             "role" : 'За что человек отвечает',
             "photo" : '',
         }
+
+        if request.path.find('lectors') != -1:
+            org['role'] = 'Регалии и место работы'
+
+        if request.path.find('partners') != -1:
+            org['role'] = ''
+            org['name'] = 'Название компании'
+            org['surname'] = 'Опрсание компании, чем она занимается'
+
+
+
         data4render.append(org)
 
 
+
     return render(request, 'main/orgs.html', {'orgs' : data4render})
+
+def dates(request):
+    dates = {
+        'dates' : ['01.06.2023 - Срок полачи заявки', '20.06.2023 - Объявление результатов отбора', '06.08.2023 - 18.08.2023 - Проведение школы'],
+    }
+    return(render(request, 'main/dates.html', dates))
+
+def text(request):
+
+    path = request.path.split(" ")[0][1:]
+    title = 'Заголовок страницы'
+    text = 'Содержание страницы'
+
+    if path == 'send_application':
+        title = 'Отравить заявку'
+        text = 'Срок отправки заявок окончек'
+
+    elif path == 'contacts':
+        title = 'Наши контакты'
+        text = """
+         Почта hippicampus.psn@gmail.com  
+         Сообщество в ВК vk.com/labson
+        """
+    elif path == 'payment':
+        title = 'Оргвзнос'
+        text = """ Участие в школе стоит 4000  руб.
+         Оргвзнос включает проживание в общежитии, кофе-брейки, фуршет по поводу открытия и закрытия школы"""
+
+    elif path == 'lodging':
+        title = 'Проживание в Пущино'
+        text = """ Участникам на время школы предоставляется место в студенческом общежитии биотехнологического факультета МГУ"""
+
+    data = {
+        'title' : title,
+        'text' : text,
+    }
+    return (render(request, 'main/text.html', data))
+
+
+
+def history(request):
+
+    data = {
+        'tiltes' : ['Школа 2023', 'Школа 2022', 'Школа 2021', 'Школа 2018'],
+        'schools' : [],
+    }
+
+    return (render(request, 'main/history.html', data))
+
+def faqs(request):
+    questions = ['Кто может участвовать?', 'Что взять с собой?', 'Куда приезжать?', 'Где проходит школа?', 'Сколько это стоит?'],
+    answers = ['Заявку может подать любой в совершенно летний', 'Ноутбук, купальные пренадлежности, хорошее настроение', 'Пущино, В 20А', 'Пущино, Институтская 3',  'ПОбробности в разделе Оргвзнос'],
+
+    faqs = zip(questions, answers)
+    data = {
+        'faqs' : faqs,
+    }
+    return (render(request, 'main/faqs.html', data))
