4c7fbe3ad60606236f0d5c1a70fcd56b0b87bd7c
ivan <imysin@mail.ru>
2024-01-08 14:32:08 +0300

    BASE_DIR is improved

--- school_site/school_site/settings.py
+++ school_site/school_site/settings.py
@@ -11,13 +11,14 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 """
 import os
 from pathlib import Path
+
+# change sqlite3 package by pysqlite3 (new version of sqlite)
 __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+BASE_DIR = Path( os.path.abspath(__file__)).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
@@ -87,9 +88,7 @@ DATABASES = {
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-
 ]
-
 
 # Password validation
 # https://docs.djangoproject.com/en/dev/ref/settings/#auth-password-validators
