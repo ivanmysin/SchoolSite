2e78322f9d588d7c15c0a3680a01fe5cf95649e9
ivan <imysin@mail.ru>
2024-02-01 14:37:23 +0300

    Deploing

--- school_site/school_site/settings.py
+++ school_site/school_site/settings.py
@@ -130,12 +130,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-# tmp = os.path.dirname(__file__)
-# tmp, _ = os.path.split(tmp)
-# tmp, _ = os.path.split(tmp)
-#
-# STATIC_ROOT = tmp + "/static/"
-STATIC_ROOT = myconfig.STATIC_ROOT
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-auto-field
